833a0b6068d9069b857b78c8a43fe78a774dcc1f
jack <543612779@qq.com>
2020-07-22 23:59:41 +0800

    [jackQ] - add - week - 15 - update  Carousel with animation

---
+++ week15/animation/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './animation/carousel.js'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  pragma: "createElement"
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development"
+};
